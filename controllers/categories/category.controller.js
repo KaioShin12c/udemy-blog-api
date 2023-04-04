2104a4667f2b7dacc30efd83c5b397b97d1bcf2f
KaioShin12c <nguyentiendat12c@gmail.com>
2023-04-04 15:05:59 +0700

    fetch all categories

--- controllers/categories/category.controller.js
+++ controllers/categories/category.controller.js
@@ -13,7 +13,17 @@ const createCategoryCtrl = async (req, res, next) => {
     next(appErr(error.message, 500));
   }
 };
-const fetchCategoriesCtrl = async (req, res, next) => {};
+const fetchCategoriesCtrl = async (req, res, next) => {
+  try {
+    const categories = await Category.find();
+    res.json({
+      status: "success",
+      data: categories,
+    });
+  } catch (error) {
+    next(appErr(error.message));
+  }
+};
 const categoryDetailsCtrl = async (req, res, next) => {};
 const deleteCategoryCtrl = async (req, res, next) => {};
 const updateCategoryCtrl = async (req, res, next) => {};
